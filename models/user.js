df3ceebd8ea9e016a06a1e116187e2f17abf79e8
vkpvivek <vkpvivek67@gmail.com>
2024-03-10 14:16:25 +0530

    Order System in mongoDB using mongoose

--- models/user.js
+++ models/user.js
@@ -58,6 +58,10 @@ userSchema.methods.deleteItemFromCart = function(productId){
 
 }
 
+userSchema.methods.clearCart = function(){
+    this.cart ={ items:[] };
+    return this.save();
+};
 
 module.exports =mongoose.model('User',userSchema);
 
